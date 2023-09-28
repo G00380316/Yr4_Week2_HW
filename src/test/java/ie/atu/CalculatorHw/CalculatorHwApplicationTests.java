fb056cd3f11aca77ec31041d7ccffc6937c667e7
Enoch Abiodun <g00380316@atu.ie>
2023-09-28 11:47:53 +0100

    Project Started by Springboot Initializer

---
+++ src/test/java/ie/atu/CalculatorHw/CalculatorHwApplicationTests.java
@@ -0,0 +1,13 @@
+package ie.atu.CalculatorHw;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CalculatorHwApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
